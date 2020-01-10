242488ea202b97eb822f02beba8ec9e74a971960
ScottThunderclaw <scottwegley@gmail.com>
2020-01-10 09:56:45 -0800

    Final Merge Adaptations

--- src/main/java/frc/robot/Constants/IDs.java
+++ src/main/java/frc/robot/Constants/IDs.java
@@ -12,13 +12,9 @@ public enum IDs {
     outtakeL(7),
     outtakeR(8),
 
-
     turkeyOneWheel(79),
     //Controllers
-    joyStick(0),
-    intakeButton(4),
-    outtakeButton(6),
-    rotationControlButton(200);
+    joyStick(0);
 
     public final int value;
     IDs(int value){this.value = value;}
